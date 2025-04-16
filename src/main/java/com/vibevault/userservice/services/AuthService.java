588e097c2ea0356dd2fdb91f8f6b07ce408a0b56
Sparsh <sparsh.raj30@gmail.com>
2025-04-16 17:11:17 +0530

    Fixed a few typos

--- src/main/java/com/vibevault/userservice/services/AuthService.java
+++ src/main/java/com/vibevault/userservice/services/AuthService.java
@@ -5,8 +5,8 @@ import com.vibevault.userservice.models.Session;
 import com.vibevault.userservice.models.User;
 
 public interface AuthService {
-    public Session login(String email, String passwoord)throws InvalidCredentialsException;
+    public Session login(String email, String password)throws InvalidCredentialsException;
     public User signup(String email, String password, String name, String phone)throws EmptyEmailException, EmptyPasswordException, EmptyPhoneException, EmailAlreadyExistsException, PhoneAlreadyExistsException, UserNotFoundException;
-    public User validateToken(String token)throws InvalidTokenException, InvalidTokenException;
+    public User validateToken(String token)throws InvalidTokenException, TokenExpiredException, UserNotFoundException;
     public void logout(String token);
 }
