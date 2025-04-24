2f887d4836a7ba6826e5debcadfcfe8c472e4e61
Sparsh <sparsh.raj30@gmail.com>
2025-04-24 19:25:11 +0530

    BEUS-6 :Updated JWT_AUDIENCE to final

--- src/main/java/com/vibevault/userservice/services/Consts.java
+++ src/main/java/com/vibevault/userservice/services/Consts.java
@@ -7,6 +7,6 @@ public class Consts {
     public static final int MIN_PASSWORD_LENGTH = 10;
     public static final int JWT_EXPIRATION_TIME = 86400000; // 1 day in milliseconds
     public static final String JWT_ISSUER = "vibevault";
-    public static String JWT_AUDIENCE = "vibevault-userservice: auth";
+    public static final String JWT_AUDIENCE = "vibevault-userservice: auth";
 
 }
